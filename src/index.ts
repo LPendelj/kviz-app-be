ebf9ce1cdcb029c0ef8968ee9cad18d2743b1f1a
Luka Pendelj <pendeljluka@gmail.com>
2024-05-01 00:27:01 +0200

    created socket that listens to the FE connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import express from "express";
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
 
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Running server')
+const cors = require('cors')
+
+const httpServer = createServer()
+
+// httpServer.use(cors);
+
+
+const socket = new Server(
+    httpServer,
+    {
+        cors: {
+            origin: '*'
+        }
+    }
+)
+
+socket.on("connection", (socket) => {
+    console.log('client connected from:')
+    console.log(socket.id)
+    socket.emit('successful-connection', 'You connected successfully!')
 })
 
-app.listen(4321, ()=>{
-    console.log('Running on 4321');
-    
+httpServer.listen(3000, ()=>{
+    console.log('Running on 3000'); 
 })
